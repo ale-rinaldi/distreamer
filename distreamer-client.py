4f3f32d1e4155d802646d8e1183cb24a727dc831
Alessandro Rinaldi <alessandro.rinaldi@globogis.it>
2016-02-26 16:47:11 +0100

    Initial commit

---
+++ distreamer-client.py
@@ -0,0 +1,192 @@
+import time
+import BaseHTTPServer
+import threading
+import urllib2
+import sys
+import ConfigParser
+import traceback
+from SocketServer import ThreadingMixIn
+
+VERSION='1.0.3'
+
+fragments={}
+reconnect=0
+
+try:
+	config = ConfigParser.ConfigParser({
+		'HOST_NAME': '0.0.0.0',
+		'PORT_NUMBER': 8080,
+		'SERVERURL': '',
+		'HTTPTIMEOUT': 5,
+		'HTTPINTERVAL': 3,
+		'DEBUG': False
+	})
+	if len(sys.argv)>1:
+		file=sys.argv[1]
+		config.read([file])
+	else:
+		config.read(['distreamer.conf'])
+except:
+	print "Cannot read configuration file"
+	if DEBUG:
+		print traceback.format_exc(sys.exc_info())
+	sys.exit(1)
+	
+HOST_NAME=config.get('CLIENT','HOST_NAME')
+PORT_NUMBER=config.getint('CLIENT','PORT_NUMBER')
+SERVERURL=config.get('CLIENT','SERVERURL')
+HTTPTIMEOUT=config.getint('CLIENT','HTTPTIMEOUT')
+HTTPINTERVAL=config.getint('CLIENT','HTTPINTERVAL')
+DEBUG=config.getboolean('CLIENT','DEBUG')
+if SERVERURL=='':
+	print "Server URL not defined. Aborting."
+	sys.exit(1)
+
+class ThreadingSimpleServer(ThreadingMixIn, BaseHTTPServer.HTTPServer):
+    pass
+
+class distreamerServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+	global fragments, DEBUG, icyint, icyheaders, reconnect
+	def do_HEAD(s):
+		s.send_response(200)
+		s.send_header("Server", "DiStreamer/"+VERSION)
+		for header in icyheaders:
+			s.send_header(header,icyheaders[header])
+		if(icyint>0):
+			s.send_header("icy-metaint", str(icyint))
+			
+	def do_GET(s):
+		s.send_response(200)
+		s.send_header("Server", "DiStreamer/"+VERSION)
+		for header in icyheaders:
+			s.send_header(header,icyheaders[header])
+		if(icyint>0):
+			s.send_header("icy-metaint", str(icyint))
+		s.end_headers()
+		if reconnect<=0:
+			return None
+		sentlist=[]
+		locreconnect=reconnect
+		firstsent=False
+		if icyint>0:
+			if locreconnect!=reconnect or reconnect<=0:
+				return None
+			while not firstsent:
+				try:
+					locallist=fragments.keys()
+					locallist.sort()
+					for fragn in locallist:
+						if icylist.has_key(fragn):
+							icyblkmin=min(icylist[fragn])
+							lastsent=fragn
+							s.wfile.write(fragments[fragn][icyblkmin:])
+							sentlist.append(fragn)
+							firstsent=True
+							break
+						else:
+							sentlist.append(fragn)
+				except:
+					time.sleep(1)
+					pass
+		while True:
+			try:
+				if locreconnect!=reconnect or reconnect<=0:
+					return None
+				tosend=''
+				locallist=fragments.keys()
+				locallist.sort()
+				for fragn in locallist:
+					if fragn not in sentlist:
+						if fragn!=lastsent+1:
+							return None
+						lastsent=fragn
+						tosend=tosend+fragments[fragn]
+						sentlist.append(fragn)
+				for sentn in sentlist:
+					if not fragments.has_key(sentn):
+						sentlist.remove(sentn)
+						if DEBUG:
+							print str(time.asctime())+": Removed from sent list", sentn
+			except:
+				pass
+			s.wfile.write(tosend)
+			time.sleep(1)
+	if not DEBUG:
+		def log_message(self, format, *args):
+			return
+
+def distreamerServerThread():
+	global VERSION, SERVERURL, HOST_NAME, PORT_NUMBER, HTTPTIMEOUT
+	server_class = ThreadingSimpleServer
+	httpd = server_class((HOST_NAME, PORT_NUMBER), distreamerServerHandler)
+	print str(time.asctime())+": Distreamer Client "+VERSION+" started"
+	print str(time.asctime())+": Server URL: "+str(SERVERURL)
+	print str(time.asctime())+": Listening on "+HOST_NAME+", port "+str(PORT_NUMBER)
+	print str(time.asctime())+": HTTP timeout: "+str(HTTPTIMEOUT)
+	print str(time.asctime())+": HTTP interval: "+str(HTTPINTERVAL)
+	try:
+		httpd.serve_forever()
+	except KeyboardInterrupt:
+		pass
+	httpd.server_close()
+	print str(time.asctime())+": Distreamer Client Stopped"
+	
+t = threading.Thread(target=distreamerServerThread)
+t.daemon=True
+t.start()
+
+icyint=0
+icylist={}
+icyheaders={}
+while True:
+	try:
+		result=urllib2.urlopen(urllib2.Request(SERVERURL+'/list',headers={'User-Agent':'DiStreamer/'+VERSION}), timeout=HTTPTIMEOUT)
+		exclen=result.headers['Content-Length']
+		cslist=result.read()
+		if len(cslist)!=int(exclen):
+			print str(time.asctime())+": Incomplete read of list"
+			raise ValueError("Incomplete read of list")
+		infolist=cslist.split('|')
+		icyint=int(infolist[1])
+		if icyint>0:
+			sicylist=infolist[2].split(',')
+			for icyfrag in sicylist:
+				aicylist=icyfrag.split(':')
+				if aicylist[0]!='':
+					icyidx=int(aicylist[0])
+					icylist[icyidx]=map(int,aicylist[1].split('-'))
+		aicyheaders=infolist[3].split(',')
+		tmplist={}
+		for header in aicyheaders:
+			seppos=header.find(':')
+			key=header[:seppos]
+			val=header[seppos+1:]
+			tmplist[key]=val
+		icyheaders=tmplist
+		reconnect=int(infolist[4])
+		cslist=infolist[0]
+		slist=cslist.split(',')
+		list=map(int,slist)
+		for fragn in list:
+			if not(fragments.has_key(fragn)) or fragments[fragn]=='':
+				result=urllib2.urlopen(urllib2.Request(SERVERURL+'/'+str(fragn),headers={'User-Agent':'DiStreamer/'+VERSION}), timeout=HTTPTIMEOUT)
+				exclen=result.headers['Content-Length']
+				fragment=result.read()
+				if len(fragment)!=int(exclen):
+					print str(time.asctime())+": Incomplete read of block"
+					raise ValueError("Incomplete read of block")
+				fragments[fragn]=fragment
+				if DEBUG:
+					print str(time.asctime())+": Downloaded fragment",fragn
+		locallist=fragments.keys()
+		for localfragn in locallist:
+			if(localfragn not in list):
+				del fragments[localfragn]
+				if DEBUG:
+					print str(time.asctime())+": Deleted fragment ",localfragn
+	except:
+		print str(time.asctime())+": Read error"
+		if DEBUG:
+			print traceback.format_exc(sys.exc_info())
+		pass
+	time.sleep(HTTPINTERVAL)
---
+++ distreamer-server.py
@@ -0,0 +1,198 @@
+import time
+import BaseHTTPServer
+import threading
+import urllib2
+import sys
+import ConfigParser
+import traceback
+from SocketServer import ThreadingMixIn
+
+VERSION='1.0.3'
+
+fragments={}
+counter=1
+reconnect=1
+
+try:
+	config = ConfigParser.ConfigParser({
+		'HOST_NAME': '0.0.0.0',
+		'PORT_NUMBER': 7080,
+		'STREAMURL': '',
+		'FRAGMENTSNUMBER': 5,
+		'FRAGMENTSIZE': 81920,
+		'GETMETADATA': True,
+		'HTTPTIMEOUT': 5,
+		'DEBUG': False
+	})
+	if len(sys.argv)>1:
+		file=sys.argv[1]
+		config.read([file])
+	else:
+		config.read(['distreamer.conf'])
+except:
+	print "Cannot read configuration file. Aborting."
+	if DEBUG:
+		print traceback.format_exc(sys.exc_info())
+	sys.exit(1)
+	
+HOST_NAME=config.get('SERVER','HOST_NAME')
+PORT_NUMBER=config.getint('SERVER','PORT_NUMBER')
+STREAMURL=config.get('SERVER','STREAMURL')
+FRAGMENTSNUMBER=config.getint('SERVER','FRAGMENTSNUMBER')
+FRAGMENTSIZE=config.getint('SERVER','FRAGMENTSIZE')
+GETMETADATA=config.getboolean('SERVER','GETMETADATA')
+HTTPTIMEOUT=config.getint('SERVER','HTTPTIMEOUT')
+DEBUG=config.getboolean('SERVER','DEBUG')
+
+if STREAMURL=='':
+	print "Stream URL not defined. Aborting."
+	sys.exit(1)
+
+class ThreadingSimpleServer(ThreadingMixIn, BaseHTTPServer.HTTPServer):
+    pass
+
+class distreamerServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+	global fragments, DEBUG, icylist, icyheaders, reconnect
+	def do_HEAD(s):
+		try:
+			if(s.path=='/list'):
+				s.send_response(200)
+				s.send_header("Content-Type", "text/plain")
+			else:
+				key=int(s.path[1:])
+				if fragments.has_key(key):
+					s.send_response(200)
+				else:
+					s.send_response(404)
+		except:
+			print str(time.asctime())+": HEAD error"
+			if DEBUG:
+				print traceback.format_exc(sys.exc_info())
+			pass
+			
+	def do_GET(s):
+		try:
+			if s.path=='/list':
+				s.send_response(200)
+				s.send_header("Server", "DiStreamer/"+VERSION)
+				s.send_header("Content-Type", "text/plain")
+				flist=fragments.keys()
+				flist.sort()
+				tosend=','.join(map(str,flist))
+				tosend=tosend+'|'+str(icyint)+'|'
+				tmplist=[]
+				for frag in icylist:
+					tmplist.append(str(frag)+':'+'-'.join(map(str,icylist[frag])))
+				tosend=tosend+','.join(tmplist)+'|'
+				tmplist=[]
+				for metaidx in icyheaders:
+					tmplist.append(metaidx+':'+icyheaders[metaidx])
+				tosend=tosend+','.join(tmplist)+'|'+str(reconnect)
+				s.send_header("Content-Length", str(len(tosend)))
+				s.end_headers()
+				s.wfile.write(tosend)
+			else:
+				key=int(s.path[1:])
+				if fragments.has_key(key):
+					s.send_response(200)
+					s.send_header("Server", "DiStreamer/"+VERSION)
+					s.send_header("Content-Length", str(len(fragments[key])))
+					s.end_headers()
+					s.wfile.write(fragments[key])
+				else:
+					s.send_response(404)
+					s.send_header("Server", "DiStreamer/"+VERSION)
+					s.end_headers()
+					s.wfile.write("Invalid fragment "+str(key))
+		except:
+			print str(time.asctime())+": GET error"
+			if DEBUG:
+				print traceback.format_exc(sys.exc_info())
+			pass
+	if not DEBUG:
+		def log_message(self, format, *args):
+			return
+
+def distreamerServerThread():
+	global VERSION, STREAMURL, HOST_NAME, PORT_NUMBER, FRAGMENTSNUMBER, FRAGMENTSIZE, HTTPTIMEOUT
+	server_class = ThreadingSimpleServer
+	httpd = server_class((HOST_NAME, PORT_NUMBER), distreamerServerHandler)
+	print str(time.asctime())+": Distreamer Server "+VERSION+" started"
+	print str(time.asctime())+": Stream URL: "+STREAMURL
+	print str(time.asctime())+": Buffering "+str(FRAGMENTSNUMBER)+" blocks of "+str(FRAGMENTSIZE)+" bytes"
+	print str(time.asctime())+": Listening on "+HOST_NAME+", port "+str(PORT_NUMBER)
+	print str(time.asctime())+": HTTP timeout: "+str(HTTPTIMEOUT)
+	try:
+		httpd.serve_forever()
+	except KeyboardInterrupt:
+		pass
+	httpd.server_close()
+	print str(time.asctime())+": Distreamer Server Stopped"
+	
+t = threading.Thread(target=distreamerServerThread)
+t.daemon=True
+t.start()
+
+
+
+while True:
+	icyint=0
+	icyread=0
+	icylist={}
+	icyheaders={}
+	try:
+		if GETMETADATA:
+			stream=urllib2.urlopen(urllib2.Request(STREAMURL,headers={'Icy-MetaData':'1','User-Agent':'DiStreamer/'+VERSION}), timeout=HTTPTIMEOUT)
+			if stream.headers.has_key('icy-metaint'):
+				icyint=int(stream.headers['icy-metaint'])
+			for metaidx in stream.headers.keys():
+				if ( metaidx[:4]=='icy-' and metaidx!='icy-metaint' ) or metaidx.lower()=='content-type':
+					icyheaders[metaidx]=stream.headers[metaidx]
+		else:
+			stream=urllib2.urlopen(urllib2.Request(STREAMURL,headers={'User-Agent':'DiStreamer/'+VERSION}), timeout=HTTPTIMEOUT)
+		while True:
+			fragment=stream.read(FRAGMENTSIZE)
+			if len(fragment)!=FRAGMENTSIZE:
+				raise ValueError("Incomplete read of block")
+			idx=counter
+			if icyint>0:
+				if len(fragment)<icyint-icyread:
+					icyread=icyread+len(fragment)
+				else:
+					x=0
+					while x<len(fragment):
+						if icyread==icyint:
+							icylen=ord(fragment[x])*16+1
+							icytblock=idx
+							icytpos=x+icylen
+							if icytpos>len(fragment):
+								icyblock=idx+1
+								icypos=icytpos-len(fragment)
+							else:
+								icyblock=idx
+								icypos=icytpos
+							if not icylist.has_key(icyblock):
+								icylist[icyblock]=[]
+							icylist[icyblock].append(icypos)
+							x=x+icylen
+							icyread=0
+						icyread=icyread+1
+						x=x+1
+			counter=counter+1
+			fragments[idx]=fragment
+			if DEBUG:
+				print str(time.asctime())+": Created fragment",idx
+			while len(fragments)>FRAGMENTSNUMBER:
+				todelete=min(fragments.keys())
+				del fragments[todelete]
+				if icylist.has_key(todelete):
+					del icylist[todelete]
+				if DEBUG:
+					print str(time.asctime())+": Deleted fragment",todelete
+	except:
+		pass
+	print str(time.asctime())+": Error reading from source server. Restarting connection."
+	if DEBUG:
+		print traceback.format_exc(sys.exc_info())
+	time.sleep(1)
+	reconnect=reconnect+1
