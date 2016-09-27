c4d08e1a7c1f6df0b7a0ffa06b506f88d70b7a8c
Alessandro Rinaldi <ale@alerinaldi.it>
2016-09-27 23:11:39 +0200

    Add semaphore for logging

--- General/DiStreamerLogger.py
+++ General/DiStreamerLogger.py
@@ -5,11 +5,16 @@ class DiStreamerLogger:
 		self.outputlevel=int(config['outputlevel'])
 		self.logfile=config['logfile']
 		self.loglevel=int(config['loglevel'])
+		self.islogging=False
 	
 	def log(self,string,origin,level):
+		while self.islogging:
+			time.sleep(0.05)
+		self.islogging=True
 		fmtstring=str(time.asctime())+": ["+origin+"] "+string
 		if level<=self.outputlevel:
 			print fmtstring
 		if level<=self.loglevel:
 			with open(self.logfile, 'a') as file:
 				file.write(fmtstring+'\r\n')
+		self.islogging=False
