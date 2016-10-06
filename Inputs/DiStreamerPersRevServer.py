200c18453182f77398bacf39b9ae2572c7a247fe
Alessandro Rinaldi <ale@alerinaldi.it>
2016-10-07 00:19:37 +0200

    Added DiStreamerPersRevClient and DiStreamerPersRevServer

---
+++ Inputs/DiStreamerPersRevServer.py
@@ -0,0 +1,135 @@
+import BaseHTTPServer, json, threading, urlparse, SocketServer
+
+class SourceServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+	pass
+
+class MetadataServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
+	pass
+
+def makePersRevServerHandler(store,logger,config,lisclosing):
+	class DiStreamerPersRevServerHandler(SocketServer.StreamRequestHandler,object):
+		timeout=config['timeout']
+		
+		def keysToInt(s,dictionary):
+			''' THANKS!!! http://stackoverflow.com/questions/1254454/fastest-way-to-convert-a-dicts-keys-values-from-unicode-to-str '''
+			if not isinstance(dictionary, dict):
+				return dictionary
+			return dict((int(k), s.keysToInt(v)) for k, v in dictionary.items())
+		
+		def handle(self):
+			password=self.rfile.readline().strip()
+			if config['password']!='' and password!=config['password']:
+				self.wfile.write('err|Invalid password\r\n')
+				return None
+			fragments=store.getFragments()
+			fkeys=fragments.keys()
+			fkeys.sort()
+			self.wfile.write('ok|'+'/'.join(map(str,fkeys))+'\r\n')
+			while not lisclosing[0]:
+				info=''
+				try:
+					info=self.rfile.readline().strip()
+				except:
+					if lisclosing[0]:
+						pass
+					else:
+						logger.log('Incomplete read of line','DiStreamerPersRevServer',2)
+						return None
+				aline=info.split('|')
+				action=aline[0]
+				try:
+					length=int(aline[1])
+				except:
+					if lisclosing[0]:
+						pass
+					else:
+						logger.log('Error in length conversion','DiStreamerPersRevServer',2)
+						return None
+				content=''
+				try:
+					content=self.rfile.read(length)
+				except:
+					if lisclosing[0]:
+						pass
+					else:
+						logger.log('Error redading '+action,'DiStreamerPersRevServer',2)
+						return None
+				if len(content)!=length:
+					logger.log('Incomplete read of '+action,'DiStreamerPersRevServer',2)
+					return None
+				try:
+					self.rfile.read(2)
+				except:
+					if lisclosing[0]:
+						pass
+					else:
+						logger.log('Error in reading final bytes','DiStreamerPersRevServer',2)
+						return None
+				if action=='err':
+					logger.log('Error from client: '+content,'DiStreamerPersRevServer',2)
+					return None
+				if action=='list':
+					infolist=json.loads(content)
+					store.setIcyInt(infolist['icyint'])
+					store.setIcyList(self.keysToInt(infolist['icylist']))
+					store.setIcyHeaders(infolist['icyheaders'])
+					store.setSourceGen(infolist['sourcegen'])
+					store.setIcyTitle(infolist['icytitle'].decode('base64'))
+					list=infolist['fragmentslist']
+					for localfragn in fragments.keys():
+						if(localfragn not in list):
+							del fragments[localfragn]
+							logger.log('Deleted fragment '+str(localfragn),'DiStreamerPersRevServer',3)
+				else:	
+					x=-1
+					try:
+						x=int(action)
+					except:
+						pass
+					if x>=0:
+						if len(fragments.keys())>0 and x>max(fragments.keys())+1:
+							logger.log('Expected fragment: '+str(max(fragments.keys()))+', received: '+str(x)+'. Resetting store.','DiStreamerPersRevServer',2)
+							store.reset()
+						fragments[x]=content
+						logger.log('Received fragment '+str(x),'DiStreamerPersRevServer',3)
+	return DiStreamerPersRevServerHandler
+
+class DiStreamerPersRevServer:
+
+	def __init__(self,store,logger):
+		self.store=store
+		self.logger=logger
+		self.config_set=False
+		self.lisclosing=[False]
+		self.srv=None
+
+	def getDefaultConfig(self):
+		return {
+			'hostname': '0.0.0.0',
+			'port': 3080,
+			'password': '',
+			'timeout': 5
+		}
+
+	def setConfig(self,config):
+		self.config=config
+		self.config_set=True
+
+	def run(self):
+		if not self.config_set:
+			self.logger.log('Config not set','DiStreamerPersRevServer',1)
+			return None
+		srvhandler=makePersRevServerHandler(self.store,self.logger,self.config,self.lisclosing)
+		self.srv=SourceServer((self.config['hostname'],self.config['port']), srvhandler)
+		self.logger.log('Server initialized','DiStreamerPersRevServer',2)
+		try:
+			self.srv.serve_forever()
+		except:
+			if self.lisclosing[0]:
+				pass
+		self.logger.log('Server terminated','DiStreamerPersRevServer',2)
+
+	def close(self):
+		self.logger.log('Closing server','DiStreamerPersRevServer',2)
+		self.lisclosing[0]=True
+		self.srv.server_close()
---
+++ Outputs/DiStreamerPersRevClient.py
@@ -0,0 +1,105 @@
+import socket,urlparse,json,time
+
+
+class DiStreamerPersRevClient():
+
+	def __init__(self,store,logger):
+		self.store=store
+		self.logger=logger
+		self.config_set=False
+		self.isclosing=False
+		self.socket=None
+		self.socketfile=None
+
+	def _PersRevServerConnect(self,url,timeout):
+		u=urlparse.urlparse(url)
+		server=u.netloc.split(':')[0]
+		port=-1
+		if len(u.netloc.split(':'))>1:
+			port=int(u.netloc.split(':')[1])
+		if port<0:
+			port=80
+		self.logger.log('Connecting to '+server+' on port '+str(port),'DiStreamerPersRevClient',4)
+		s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.setblocking(1)
+		s.connect((server, port))
+		self.logger.log('Connected','DiStreamerPersRevClient',4)
+		f=s.makefile()
+		s.settimeout(timeout)
+		self.socket=s
+		self.socketfile=f
+		return f
+		
+	def getDefaultConfig(self):
+		return {
+			'serverurl': '',
+			'password': '',
+			'httptimeout': 5,
+			'interval': 1
+		}
+	
+	def setConfig(self,config):
+		self.config=config
+		self.config_set=True
+
+	def run(self):
+		if not self.config_set:
+			self.logger.log('Config not set','DiStreamerPersRevClient',1)
+			return None
+
+		if self.config['serverurl']=='':
+			self.logger.log('Server URL not defined','DiStreamerPersRevClient',1)
+			return None
+
+		self.logger.log('Started','DiStreamerPersRevClient',2)
+		
+		stream=self._PersRevServerConnect(self.config['serverurl']+'/',self.config['httptimeout'])
+		self.socket.sendall(self.config['password']+'\r\n')
+		ans=stream.readline()
+		aans=ans.split('|')
+		if aans[0]!='ok':
+			self.logger.log('Error from server: '+aans[1],'DiStreamerPersRevClient',2)
+			return None
+		self.logger.log("Connected to server",'DiStreamerPersRevClient',3)
+		sentlist=aans[1].split('/')
+		
+		fragments=self.store.getFragments()
+		if len(fragments.keys())>0:
+			lastsent=min(fragments.keys())-1
+		else:
+			lastsent=0
+		while not self.isclosing:
+			tosend=''
+			locallist=fragments.keys()
+			locallist.sort()
+			list=json.dumps({
+				'fragmentslist': locallist,
+				'icyint': self.store.getIcyInt(),
+				'icylist': self.store.getIcyList(),
+				'icyheaders': self.store.getIcyHeaders(),
+				'icytitle': self.store.getIcyTitle().encode('base64'),
+				'sourcegen': self.store.getSourceGen()
+			})
+			for fragn in locallist:
+				if fragn not in sentlist:
+					if fragn!=lastsent+1:
+						self.logger.log('Expected fragment: '+str(lastsent+1)+', first available: '+str(fragn)+'. Closing stream to server.','DiStreamerPersRevClient',2)
+						return None
+					tosend+=str(fragn)+'|'+str(len(fragments[fragn]))+'\r\n'+fragments[fragn]+'\r\n'
+					lastsent=fragn
+					self.logger.log('Sent fragment '+str(fragn),'DiStreamerPersRevClient',3)
+					sentlist.append(fragn)
+			tosend+='list|'+str(len(list))+'\r\n'+list+'\r\n'
+			for sentn in sentlist:
+				if not sentn in locallist:
+					sentlist.remove(sentn)
+					self.logger.log('Removed from sent list: '+str(sentn),'DiStreamerPersRevClient',4)
+			self.socket.sendall(tosend)
+			time.sleep(self.config['interval'])
+		self.logger.log('DiStreamerPersRevClient terminated normally','DiStreamerPersRevClient',2)
+		
+	def close(self):
+		self.isclosing=True
+		self.logger.log('DiStreamerPersRevClient is terminating, this could need some time','DiStreamerPersRevClient',2)
+		if self.socket:
+			self.socket.close()
