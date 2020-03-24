bd0bb3b4750aa9295488a3bcaf4c9acb7727b2f0
Alessandro Rinaldi <ale.rinaldi@criptalia.it>
2020-03-24 22:19:21 +0100

    Fix timeouts

--- Outputs/DiStreamerPersRevClient.py
+++ Outputs/DiStreamerPersRevClient.py
@@ -22,10 +22,13 @@ class DiStreamerPersRevClient():
         self.logger.log('Connecting to ' + server + ' on port ' + str(port),'DiStreamerPersRevClient', 4)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.IPPROTO_IP, socket.IP_TOS, 0x40)
-        s.setblocking(1)
+        # Set the timeout a first time for the connection
+        s.settimeout(timeout)
         s.connect((server, port))
+        s.setblocking(1)
         self.logger.log('Connected','DiStreamerPersRevClient',4)
         f = s.makefile()
+        # Set the timeout again after setting blocking mode
         s.settimeout(timeout)
         self.socket = s
         self.socketfile = f
